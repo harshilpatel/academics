800defdf357d0e1cb583e689878d2a5e3cb36c8f
harshilpatel <harshil912@gmail.com>
2016-08-09 00:26:54 +0530

    .

--- cryptography/hill_cipher.py
+++ cryptography/hill_cipher.py
@@ -1,2 +1,8 @@
 p = raw_input("Plaintext: ")
-key = raw_input("KEY: ")
+x = input("Input key matrix rows")
+x = int(x)
+
+key = []
+
+for i in range(x):
+	temp = [int(i) for i in raw_input("Row(" + str(i) +")").split()]
